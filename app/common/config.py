36a2476abfa326594792a202a8b89512d8070698
爱搞机的学生_XUESHENG <qq.com3420510534@outlook.com>
2026-07-26 17:08:38 +0800

    chore(release): prepare for v5.0.0-pre.13

--- app/common/config.py
+++ app/common/config.py
@@ -1,6 +1,6 @@
 from app.config.cfg import cfg
 
 # GitHub Actions extracts this assignment as text before importing the app.
-VERSION = "5.0.0-pre.12"
+VERSION = "5.0.0-pre.13"
 
 __all__ = ["VERSION", "cfg"]
